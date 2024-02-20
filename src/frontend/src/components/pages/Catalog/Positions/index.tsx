f909005f08eb5d134d87f5aad9b11af88df5b7d7
Tryd0g0lik <work80@mail.ru>
2024-02-20 10:00:20 +0700

    [01.1]: was beeb added a component 'pages\Catalog\Positions' for page /catalog'

---
+++ src/frontend/src/components/pages/Catalog/Positions/index.tsx
@@ -0,0 +1,28 @@
+import React, { Children, JSX } from 'react';
+import { Position } from '@Root';
+import AncorFC from '@Attribute/Ancor.tsx';
+
+/**
+ * file `src\frontend\src\components\pages\Catalog\Positions\index.tsx`
+ *
+ * `import { PositionFC } from '../Positions/index.tsx';`
+ *
+ * @param `title`: `string`. It is a name position
+ * @param `price?`: `string` Default values is '0'. This is the main price.
+ * @param `children`: `React.JSX.Element`
+ * @returns
+ */
+export function PositionFC({ title, price = '0', children }: Position): JSX.Element {
+  return (
+    <div className="col-4">
+      <div className="card catalog-item-card">
+        {children}
+        <div className="card-body">
+          <p className="card-text">{title}</p>
+          <p className="card-text">{price} руб.</p>
+          <AncorFC path='/products/1.html' classes='btn btn-outline-primary' context='Заказать' />
+        </div>
+      </div>
+    </div>
+  );
+}
