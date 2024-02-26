8745afbf2aefe5041aa77572008f3a787b2e6f1e
Tryd0g0lik <work80@mail.ru>
2024-02-26 13:28:46 +0700

    [01.3]: Here a component 'UseMainFC'src\frontend\src\components\pages\Home\Main\index.tsx' has been  code corrected

--- src/frontend/src/components/pages/Home/Main/index.tsx
+++ src/frontend/src/components/pages/Home/Main/index.tsx
@@ -9,6 +9,8 @@ import { PositionFC } from '@site/Positions/index.tsx';
 import { SFetch } from '@service/server.ts';
 import { HandlerPositionVal } from '@type';
 import UseCategoriesFC from '@site/Categories.tsx';
+import DivFC from '@site/Div.tsx';
+import ButtonFC from '@site/Forms/Button.tsx';
 
 const REACT_APP_URL = process.env.REACT_APP_URL as string;
 const REACT_APP_BPORT = process.env.REACT_APP_BPORT as string;
@@ -24,7 +26,7 @@ export function UseMainFC(): JSX.Element {
   /* This datas  is a state for the top-sales */
   const [topsales, useTopsales] = useState<HandlerPositionVal>();
   const [category, useCategory] = useState<HandlerPositionVal>();
-  const [catalog, useCatalog] = useState<HandlerPositionVal>();
+  const [positions, usePositions] = useState<HandlerPositionVal>();
 
   useEffect(() => {
     const serverTopSales = new SFetch(url);
@@ -38,6 +40,12 @@ export function UseMainFC(): JSX.Element {
     serverCategory.requestOneBefore = { categories: true };
     serverCategory.requestOneParamAsync(useCategory);
   }, [useCategory]);
+  useEffect(() => {
+    const serverPositions = new SFetch(url);
+    /* create a request to the server */
+    serverPositions.requestOneBefore = { offset: 3 };
+    serverPositions.requestOneParamAsync(usePositions);
+  }, [usePositions]);
 
   return (
     <main className="container">
@@ -55,7 +63,12 @@ export function UseMainFC(): JSX.Element {
                   <div className="row">
                     {Array.from(topsales).map((obj) => (
                       <PositionFC key={obj.id} title={obj.title} price={obj.price} >
-                        <ImageFC path={(obj.images as string[])[0]} classes='card-img-top img-fluid' context={obj.title} />
+                        <ImageFC path={
+                          ((obj.images !== undefined) &&
+                            (obj.images.length > 0))
+                            ? obj.images[0]
+                            : '#'
+                        } classes='card-img-top img-fluid' context={obj.title} />
                       </PositionFC>
                     ))
                     }
@@ -72,10 +85,35 @@ export function UseMainFC(): JSX.Element {
                   <UseCategoriesFC {...category} />
                 )
                 : (
-                  <></>
+                  < ImLoader />
                 )
             }
-            <ImLoader />
+
+            <div className="row">
+              {/* This is simply positions */}
+              {
+                (positions !== undefined)
+                  ? (
+                    Array.from(positions).map((obj) => (
+                      <PositionFC key={obj.id} title={obj.title} price={obj.price}>
+                        <Fragment>
+                          <ImageFC path={
+                            ((obj.images !== undefined) &&
+                              (obj.images.length > 0))
+                              ? obj.images[0]
+                              : '#'
+                          } classes='card-img-top img-fluid' context={obj.title} />
+                        </Fragment>
+                      </PositionFC>
+
+                    ))
+                  )
+                  : < ImLoader />
+              }
+            </div>
+            <DivFC classes='text-center'>
+              <ButtonFC classes='btn btn-outline-primary' context='Загрузить ещё' />
+            </DivFC>
           </section>
         </div>
       </div>
