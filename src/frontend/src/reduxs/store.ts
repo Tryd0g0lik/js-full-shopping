a5dac95a94f51adb42426701b72ae01b54065466
Tryd0g0lik <work80@mail.ru>
2024-03-01 11:53:03 +0700

    [2]: the 'import { thunk } from 'redux-thunk';' has been install and rewrited the 'middlewareEnhancer', composeWithDevTools

--- src/frontend/src/reduxs/store.ts
+++ src/frontend/src/reduxs/store.ts
@@ -3,19 +3,28 @@
 // https://redux.js.org/tutorials/quick-start#create-a-redux-store
 // https://redux.js.org/tutorials/quick-start#add-slice-reducers-to-the-store
 
-import { compose, legacy_createStore as createStore, combineReducers } from 'redux';
+import { compose, legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
 import counterReducer from './reducers.ts';
+// import { applyMiddleware } from '@reduxjs/toolkit';
 // const REACT_APP_REDUX_DEVTOOLS = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const middlewareEnhancer = applyMiddleware(thunk);
+
+const composeWithDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composedEnhancers = composeWithDevTools(middlewareEnhancer);
 
 const legacyCreateStire = createStore(
   combineReducers({
     categories: counterReducer
   }),
   compose(
-    composeEnhancers
+    composedEnhancers
   )
 );
 
@@ -23,7 +32,7 @@ const configStore = (): typeof legacyCreateStire => {
   return legacyCreateStire;
 };
 // const configStore = (): void => { };
-export default configStore;
+export default legacyCreateStire;
 
 // let devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__?.();
 // if (devToolsExtension !== null) {
